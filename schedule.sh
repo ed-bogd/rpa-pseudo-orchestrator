52412995484d30e5f708841250bd6e3235f8d190
ed <stegarex@gmail.com>
2025-03-03 14:05:01 +0000

    init

---
+++ schedule.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+create_scheduled_task() {
+    local task_name="$1"
+    local script_path="$2"
+    local schedule="$3"
+    
+    (crontab -l 2>/dev/null; echo "$schedule $script_path # $task_name") | crontab -
+}
+
+delete_scheduled_task() {
+    local task_name="$1"
+    crontab -l | grep -v "# $task_name" | crontab -
+}
+
+enable_scheduled_task() {
+    local task_name="$1"
+    crontab -l | sed "s/^#\([^#]*\)# $task_name$/\1# $task_name/" | crontab -
+}
+
+disable_scheduled_task() {
+    local task_name="$1"
+    crontab -l | sed "s/^\([^#][^#]*\)# $task_name$/#\1# $task_name/" | crontab -
+}
+
+run_scheduled_task() {
+    local task_name="$1"
+    local script_path=$(crontab -l | grep "# $task_name$" | awk '{print $6}')
+    if [ ! -z "$script_path" ]; then
+        bash "$script_path"
+    fi
+}
