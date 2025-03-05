094a4001e1dc47e2c3428829d1c31172d4375294
ed <stegarex@gmail.com>
2025-03-05 21:09:39 +0000

    fix

--- schedule.sh
+++ schedule.sh
@@ -5,28 +5,28 @@ create_scheduled_task() {
     local script_path="$2"
     local schedule="$3"
     
-    (crontab -l 2>/dev/null; echo "$schedule $script_path # $task_name") | crontab -
+    (crontab -l 2>/dev/null; echo "# $task_name \n";  echo "$schedule $script_path") | crontab -
 }
 
-delete_scheduled_task() {
-    local task_name="$1"
-    crontab -l | grep -v "# $task_name" | crontab -
-}
+# delete_scheduled_task() {
+#     local task_name="$1"
+#     crontab -l | grep -v "# $task_name" | crontab -
+# }
 
-enable_scheduled_task() {
-    local task_name="$1"
-    crontab -l | sed "s/^#\([^#]*\)# $task_name$/\1# $task_name/" | crontab -
-}
+# enable_scheduled_task() {
+#     local task_name="$1"
+#     crontab -l | sed "s/^#\([^#]*\)# $task_name$/\1# $task_name/" | crontab -
+# }
 
-disable_scheduled_task() {
-    local task_name="$1"
-    crontab -l | sed "s/^\([^#][^#]*\)# $task_name$/#\1# $task_name/" | crontab -
-}
+# disable_scheduled_task() {
+#     local task_name="$1"
+#     crontab -l | sed "s/^\([^#][^#]*\)# $task_name$/#\1# $task_name/" | crontab -
+# }
 
-run_scheduled_task() {
-    local task_name="$1"
-    local script_path=$(crontab -l | grep "# $task_name$" | awk '{print $6}')
-    if [ ! -z "$script_path" ]; then
-        bash "$script_path"
-    fi
-}
+# run_scheduled_task() {
+#     local task_name="$1"
+#     local script_path=$(crontab -l | grep "# $task_name$" | awk '{print $6}')
+#     if [ ! -z "$script_path" ]; then
+#         bash "$script_path"
+#     fi
+# }
